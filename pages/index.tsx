9b940e120b110312f0d1ba078a360d9647b0f5d0
Lewis Rotchell <rotchell92@gmail.com>
2021-10-27 23:15:08 +0100

    Added product types and rendered on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,72 @@
-import type { InferGetStaticPropsType } from "next";
+import {
+  CardActionArea,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import type {
+  InferGetStaticPropsType,
+  InferGetServerSidePropsType,
+} from "next";
 import { Layout } from "../components/Layout";
+import { Product } from "../types/product";
 
 export default function Home({
   products,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(products);
+
+  <p>{products[0].name}</p>;
+
   return (
     <Layout>
-      <div>{products}</div>
+      <div>
+        <h1>Latest Products</h1>
+
+        <Grid container spacing={3}>
+          {products.map((product: Product) => {
+            return (
+              <Grid item md={4} key={product.id}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image.formats.medium.url}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Typography>£{product.price}</Typography>
+                    <Button size="small" color="primary">
+                      Add to cart
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </div>
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const products = [1, 2, 3];
+export async function getServerSideProps() {
+  const res = await fetch(
+    `${process.env.API_URL}/products?_sort=created_at:ASC&_limit=6`
+  );
+  const products = await res.json();
 
   return {
     props: {
       products,
     },
-    revalidate: 1,
   };
 }
---
+++ types/product.ts
@@ -0,0 +1,34 @@
+// export interface ImageFormats {
+//   large: string;
+//   medium: string;
+//   small: string;
+//   thumbnail: string;
+// }
+
+// export interface Image {
+//   formats: {
+//     [key: string]: { [key: string]: ImageFormats };
+//   };
+// }
+
+interface Sizes {
+  url: string;
+}
+
+interface Formats {
+  medium: Sizes;
+}
+
+interface Image {
+  formats: Formats;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: Image;
+  slug: string;
+  description: string;
+  created_at: string;
+}
